a97a814b6c20c3c7dc6fc947114c871886771dc7
Afraz Siddiqui <afsiddiq@microsoft.com>
2021-03-18 08:55:44 -0400

    Initial Commit

---
+++ CoreMLDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreMLDemo
+//
+//  Created by Afraz Siddiqui on 3/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
