eabfa026c7967b428a5e7196dde9594d0003d3d0
Afraz Siddiqui <afsiddiq@microsoft.com>
2021-03-18 09:14:49 -0400

    Main

---
+++ CoreMLDemo/Extensions.swift
@@ -0,0 +1,84 @@
+//
+//  Extensions.swift
+//  Test
+//
+//  Created by Afraz Siddiqui on 3/18/21.
+//
+
+import UIKit
+
+extension UIImage {
+    /// Resize image
+    /// - Parameter size: Size to resize to
+    /// - Returns: Resized image
+    func resize(size: CGSize) -> UIImage? {
+        UIGraphicsBeginImageContext(size)
+        draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+
+    /// Create and return CoreVideo Pixel Buffer
+    /// - Returns: Pixel Buffer
+    func getCVPixelBuffer() -> CVPixelBuffer? {
+        guard let image = cgImage else {
+             return nil
+        }
+        let imageWidth = Int(image.width)
+        let imageHeight = Int(image.height)
+
+        let attributes : [NSObject:AnyObject] = [
+            kCVPixelBufferCGImageCompatibilityKey : true as AnyObject,
+            kCVPixelBufferCGBitmapContextCompatibilityKey : true as AnyObject
+        ]
+
+        var pxbuffer: CVPixelBuffer? = nil
+        CVPixelBufferCreate(
+            kCFAllocatorDefault,
+            imageWidth,
+            imageHeight,
+            kCVPixelFormatType_32ARGB,
+            attributes as CFDictionary?,
+            &pxbuffer
+        )
+
+        if let _pxbuffer = pxbuffer {
+            let flags = CVPixelBufferLockFlags(rawValue: 0)
+            CVPixelBufferLockBaseAddress(_pxbuffer, flags)
+            let pxdata = CVPixelBufferGetBaseAddress(_pxbuffer)
+
+            let rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+            let context = CGContext(
+                data: pxdata,
+                width: imageWidth,
+                height: imageHeight,
+                bitsPerComponent: 8,
+                bytesPerRow: CVPixelBufferGetBytesPerRow(_pxbuffer),
+                space: rgbColorSpace,
+                bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue
+            )
+
+            if let _context = context {
+                _context.draw(
+                    image,
+                    in: CGRect.init(
+                        x: 0,
+                        y: 0,
+                        width: imageWidth,
+                        height: imageHeight
+                    )
+                )
+            }
+            else {
+                CVPixelBufferUnlockBaseAddress(_pxbuffer, flags);
+                return nil
+            }
+
+            CVPixelBufferUnlockBaseAddress(_pxbuffer, flags);
+            return _pxbuffer;
+        }
+
+        return nil
+    }
+}
--- CoreMLDemo/ViewController.swift
+++ CoreMLDemo/ViewController.swift
@@ -5,15 +5,96 @@
 //  Created by Afraz Siddiqui on 3/18/21.
 //
 
+import CoreML
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "photo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Select Image"
+        label.numberOfLines = 0
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(label)
+        view.addSubview(imageView)
+
+        let tap = UITapGestureRecognizer(
+            target: self,
+            action: #selector(didTapImage)
+        )
+        tap.numberOfTapsRequired = 1
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tap)
     }
 
+    @objc func didTapImage() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        present(picker, animated: true)
+    }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageView.frame = CGRect(
+            x: 20,
+            y: view.safeAreaInsets.top,
+            width: view.frame.size.width-40,
+            height: view.frame.size.width-40)
+        label.frame = CGRect(
+            x: 20,
+            y: view.safeAreaInsets.top+(view.frame.size.width-40)+10,
+            width: view.frame.size.width-40,
+            height: 100
+        )
+    }
+
+    private func analyzeImage(image: UIImage?) {
+        guard let buffer = image?.resize(size: CGSize(width: 224, height: 224))?
+                .getCVPixelBuffer() else {
+            return
+        }
+
+        do {
+            let config = MLModelConfiguration()
+            let model = try GoogLeNetPlaces(configuration: config)
+            let input = GoogLeNetPlacesInput(sceneImage: buffer)
+
+            let output = try model.prediction(input: input)
+            let text = output.sceneLabel
+            label.text = text
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    // Image Picker
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // cancelled
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        imageView.image = image
+        analyzeImage(image: image)
+    }
 }
 
